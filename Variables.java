fc0d3c1ad274888d252d6bdb90e94b84504b2dd6
Akash <akash5ravada@gmail.com>
2026-06-14 00:47:13 +0530

    Some intro and flows and code 😊

---
+++ Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Syntax:
+        int age = 22; //age is an variable which stores 22 value
+        String name = "Akash"; //name is an variable which stores text(Akash) value
+        double salary = 220022.256; // salary is an variable which stores decimel type value
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+    }
+}
