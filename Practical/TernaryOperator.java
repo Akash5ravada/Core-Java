8da50ac27150c477422295057a0e38fe36af893a
Akash <akash5ravada@gmail.com>
2026-06-14 19:32:32 +0530

    More easiyer than if else but less used is Ternary Operator 👍

---
+++ Practical/TernaryOperator.java
@@ -0,0 +1,17 @@
+package Practical;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int age = 22;
+        String name = "Akash";
+        double salary = 220022.256;
+
+        // Using ternary operator to check if age is greater than 18
+        String eligibility = (age > 18) ? "Eligible" : "Not Eligible";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Eligibility: " + eligibility);
+    }
+}
