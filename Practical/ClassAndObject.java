8853a93a5cd61f292f688f961672b9ae05260894
Akash <akash5ravada@gmail.com>
2026-06-15 11:40:10 +0530

    now serious!! Main topic classes object 😊

---
+++ Practical/ClassAndObject.java
@@ -0,0 +1,29 @@
+package Practical;
+
+class Car{
+    String make;
+    String model;
+    int year;
+    public void displayInfo() {
+        System.out.println("Make: " + make);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+    
+}
+
+
+public class ClassAndObject {
+    public static void main(String[] args) {
+        // Create an object of the class
+        Car myCar = new Car();
+
+        // Set the attributes of the car
+        myCar.make = "Toyota";
+        myCar.model = "Camry";
+        myCar.year = 2020;
+
+        // Call the method to display car information
+        myCar.displayInfo();
+    }
+}
